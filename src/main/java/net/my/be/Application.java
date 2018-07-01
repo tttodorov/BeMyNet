78b2e17f367c89c7a7de1c7ddd6ab83c642b2e71
Toshko Todorov <toshko.todorov@sap.com>
2018-07-01 18:16:19 +0300

    Add project

---
+++ src/main/java/net/my/be/Application.java
@@ -0,0 +1,13 @@
+package net.my.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
