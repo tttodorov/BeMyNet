7788dd3d9899f050b351dc4f23664dea80d887c3
Toshko Todorov <toshko.todorov@sap.com>
2018-07-02 15:48:05 +0300

    Add web structure

--- src/main/java/net/my/be/model/User.java
+++ src/main/java/net/my/be/model/User.java
@@ -2,11 +2,16 @@ package net.my.be.model;
 
 import lombok.Data;
 
-import javax.inject.Named;
-
 @Data
 public class User {
 	private Integer id;
+	private String email;
+	private String password;
+	private Integer birthYear;
+	private Integer birthMonth;
+	private Integer birthDay;
 	private String firstName;
 	private String lastName;
+	private String photo;
+	private Boolean sex;
 }
